8a0be30db91f0d0b1ae117d7f6c6b633d63a0ac9
gli <gli@redhat.com>
2015-03-11 23:54:16 +0800

    jsoup: Strings Util partially done

--- src/main/scala/com/github/ligangty/scala/jsoup/helper/Strings.scala
+++ src/main/scala/com/github/ligangty/scala/jsoup/helper/Strings.scala
@@ -32,13 +32,75 @@ final object Strings {
    */
   def padding(width: Integer): String = {
     if (width < 0) throw new IllegalArgumentException("width must be > 0")
-    if (width < paddings.length) paddings(width)
-    val out: Array[Char] = new Array[Char](width)
-    var i: Int = 0
-    for (i <- 0 to width - 1) {
-      out(i) = ' '
-    }
-    String.valueOf(out)
+    if (width < paddings.length) paddings(width) else " " * width
+  }
+
+  /**
+   * Tests if a code point is "whitespace" as defined in the HTML spec.
+   * @param c code point to test
+   * @return true if code point is whitespace, false otherwise
+   */
+  def isWhitespace(c: Int): Boolean =
+    c == ' ' || c == '\t' || c == '\n' || c == '\f' || c == '\r'
+
+  /**
+   * Tests if a string is blank: null, emtpy, or only whitespace (" ", \r\n, \t, etc)
+   * @param string string to test
+   * @return if string is blank
+   */
+  def isBlank(string: String): Boolean =
+    if (string == null) true else string.forall(isWhitespace(_))
+
+  /**
+   * Tests if a string is numeric, i.e. contains only digit characters
+   * @param string string to test
+   * @return true if only digit chars, false if empty or null or contains non-digit chrs
+   */
+  def isNumeric(string: String): Boolean =
+    if (string == null) false else string.forall(Character.isDigit(_))
+
+  /**
+   * Normalise the whitespace within this string multiple spaces collapse to a single, and all whitespace characters
+   * (e.g. newline, tab) convert to a simple space
+   * @param string content to normalise
+   * @return normalised string
+   */
+  def normaliseWhitespace(string: String): String = {
+    val sb = new StringBuilder(string.length())
+    appendNormalisedWhitespace(sb, string, false)
+    return sb.toString()
+  }
+
+  /**
+   * After normalizing the whitespace within a string, appends it to a string builder.
+   * @param accum builder to append to
+   * @param string string to normalize whitespace within
+   * @param stripLeading set to true if you wish to remove any leading whitespace
+   */
+  def appendNormalisedWhitespace(accum: StringBuilder, string: String, stripLeading: Boolean) {
+//    var lastWasWhite = false
+//    var reachedNonWhite = false
+//
+//    val len = string.length()
+//    var i = 0
+//    var c = 0
+//    for (i <- 0 to len - 1)
+//      i < len
+//    i += Character.charCount(c))
+//    {
+//      c = string.codePointAt(i)
+//      if (isWhitespace(c)) {
+//        if ((stripLeading && !reachedNonWhite) || lastWasWhite)
+//          continue
+//        accum.append(' ')
+//        lastWasWhite = true
+//      }
+//      else {
+//        accum.appendCodePoint(c)
+//        lastWasWhite = false
+//        reachedNonWhite = true
+//      }
+//    }
   }
 
 }
--- src/test/scala/com/github/ligangty/scala/jsoup/helper/StringsTest.scala
+++ src/test/scala/com/github/ligangty/scala/jsoup/helper/StringsTest.scala
@@ -1,27 +1,77 @@
 package com.github.ligangty.scala.jsoup.helper
 
+import com.github.ligangty.scala.jsoup.helper.Strings._
 import org.scalatest.FunSuite
+
 
 /**
  * Created by gli on 3/11/15.
  */
 class StringsTest extends FunSuite {
   test("join Iterable") {
-    assert("" == Strings.join(List(""), " "))
-    assert("one" == Strings.join(List("one"), " "))
-    assert("one two three" == Strings.join(List("one", "two", "three"), " "))
+    assertResult("")(join(List(""), " "))
+    assertResult("one")(join(List("one"), " "))
+    assertResult("one two three")(join(List("one", "two", "three"), " "))
   }
 
   test("join Array") {
-    assert("" == Strings.join(Array(""), " "))
-    assert("one" == Strings.join(Array("one"), " "))
-    assert("one two three" == Strings.join(Array("one", "two", "three"), " "))
+    assertResult("")(join(Array(""), " "))
+    assertResult("one")(join(Array("one"), " "))
+    assertResult("one two three")(join(Array("one", "two", "three"), " "))
   }
 
   test("padding") {
-    assert("" == Strings.padding(0))
-    assert(" " == Strings.padding(1))
-    assert("  " == Strings.padding(2))
-    assert("               " == Strings.padding(15))
+    assertResult("")(padding(0))
+    assertResult(" ")(padding(1))
+    assertResult("  ")(padding(2))
+    assertResult("               ")(padding(15))
+  }
+
+  test("isWhitespace") {
+    assert(isWhitespace('\t'))
+    assert(isWhitespace('\n'))
+    assert(isWhitespace('\r'))
+    assert(isWhitespace('\f'))
+    assert(isWhitespace(' '))
+    assert(!isWhitespace('\u00a0'))
+    assert(!isWhitespace('\u2000'))
+    assert(!isWhitespace('\u3000'))
+  }
+
+  test("isBlank") {
+    assert(isBlank(null))
+    assert(isBlank(""))
+    assert(isBlank("      "))
+    assert(isBlank("   \r\n  "))
+
+    assert(!isBlank("hello"))
+    assert(!isBlank("   hello   "))
+  }
+
+  test("isNumeric") {
+    assert(!isNumeric(null))
+    assert(!isNumeric(" "))
+    assert(!isNumeric("123 546"))
+    assert(!isNumeric("hello"))
+    assert(!isNumeric("123.334"))
+
+    assert(isNumeric("1"))
+    assert(isNumeric("1234"))
+  }
+
+  test("normaliseWhiteSpace") {
+    assertResult(" ")(normaliseWhitespace("    \r \n \r\n"))
+    assertResult(" hello there ")(normaliseWhitespace("   hello   \r \n  there    \n"))
+    assertResult("hello")(normaliseWhitespace("hello"))
+    assertResult("hello there")(normaliseWhitespace("hello\nthere"))
+  }
+
+ test("normaliseWhiteSpaceHandlesHighSurrogates") {
+    val test71540chars = "\ud869\udeb2\u304b\u309a  1"
+    val test71540charsExpectedSingleWhitespace = "\ud869\udeb2\u304b\u309a 1"
+
+    assertResult(test71540charsExpectedSingleWhitespace)(normaliseWhitespace(test71540chars))
+//    val extractedText = Jsoup.parse(test71540chars).text()
+//    assertResult(test71540charsExpectedSingleWhitespace)(extractedText)
   }
 }
