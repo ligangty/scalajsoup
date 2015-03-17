0144ac7b369ff64bf594833bc273937df40328ed
gli <gli@redhat.com>
2015-03-18 00:32:25 +0800

    jsoup: Document.OutputSettings done

--- src/main/scala/com/github/ligangty/scala/jsoup/nodes/Document.scala
+++ src/main/scala/com/github/ligangty/scala/jsoup/nodes/Document.scala
@@ -2,6 +2,7 @@ package com.github.ligangty.scala.jsoup.nodes
 
 import java.nio.charset.{CharsetEncoder, Charset}
 
+import com.github.ligangty.scala.jsoup.helper.Validator
 import com.github.ligangty.scala.jsoup.parser.Tag
 
 /**
@@ -15,22 +16,24 @@ class Document private(baseUri: String, location: String) extends Element(Tag("#
 
 object Document {
 
-  class OutputSettings {
+  class OutputSettings extends Cloneable {
 
     object Syntax extends Enumeration {
       type Syntax = Value
       val html, xml = Value
     }
 
+    type Syntax = Syntax.Syntax
+
     import Entities._
 
-    private var escapeModeValue: EscapeMode = BASE
-    private val charset: Charset = Charset.forName("UTF-8")
-    private val charsetEncoder: CharsetEncoder = charset.newEncoder
-    private val prettyPrint: Boolean = true
-    private val outline: Boolean = false
-    private val indentAmount: Int = 1
-    private val syntax = Syntax.html
+    private var escapeModeVal: EscapeMode = BASE
+    private var charsetVal: Charset = Charset.forName("UTF-8")
+    private var charsetEncoder: CharsetEncoder = charsetVal.newEncoder
+    private var prettyPrintVal: Boolean = true
+    private var outlineVal: Boolean = false
+    private var indentAmountVal: Int = 1
+    private var syntaxVal: OutputSettings#Syntax = Syntax.html
 
     /**
      * Get the document's current HTML escape mode: <code>base</code>, which provides a limited set of named HTML
@@ -40,7 +43,7 @@ object Document {
      * The default escape mode is <code>base</code>.
      * @return the document's current escape mode
      */
-    def escapeMode: EscapeMode = escapeModeValue
+    def escapeMode: EscapeMode = escapeModeVal
 
     /**
      * Set the document's escape mode, which determines how characters are escaped when the output character set
@@ -49,8 +52,126 @@ object Document {
      * @return the document's output settings, for chaining
      */
     def escapeMode(escapeMode: EscapeMode): Document.OutputSettings = {
-      this.escapeModeValue = escapeMode
+      this.escapeModeVal = escapeMode
       this
+    }
+
+    /**
+     * Get the document's current output charset, which is used to control which characters are escaped when
+     * generating HTML (via the <code>html()</code> methods), and which are kept intact.
+     * <p>
+     * Where possible (when parsing from a URL or File), the document's output charset is automatically set to the
+     * input charset. Otherwise, it defaults to UTF-8.
+     * @return the document's current charset.
+     */
+    def charset: Charset = charsetVal
+
+    /**
+     * Update the document's output charset.
+     * @param charSet the new charset to use.
+     * @return the document's output settings, for chaining
+     */
+    def charset(charSet: Charset): Document.OutputSettings = {
+      this.charsetVal = charSet
+      charsetEncoder = charSet.newEncoder
+      this
+    }
+
+    /**
+     * Update the document's output charset.
+     * @param charSet the new charset (by name) to use.
+     * @return the document's output settings, for chaining
+     */
+    def charset(charSet: String): Document.OutputSettings = {
+      charset(Charset.forName(charSet))
+      this
+    }
+
+    private[nodes] def encoder: CharsetEncoder = charsetEncoder
+
+    /**
+     * Get the document's current output syntax.
+     * @return current syntax
+     */
+    def syntax: OutputSettings#Syntax = syntaxVal
+
+    /**
+     * Set the document's output syntax. Either {@code html}, with empty tags and boolean attributes (etc), or
+     * {@code xml}, with self-closing tags.
+     * @param syntaxIn serialization syntax
+     * @return the document's output settings, for chaining
+     */
+    def syntax(syntaxIn: Document.OutputSettings#Syntax): Document.OutputSettings = {
+      this.syntaxVal = syntaxIn
+      this
+    }
+
+    /**
+     * Get if pretty printing is enabled. Default is true. If disabled, the HTML output methods will not re-format
+     * the output, and the output will generally look like the input.
+     * @return if pretty printing is enabled.
+     */
+    def prettyPrint: Boolean = prettyPrintVal
+
+    /**
+     * Enable or disable pretty printing.
+     * @param pretty new pretty print setting
+     * @return this, for chaining
+     */
+    def prettyPrint(pretty: Boolean): Document.OutputSettings = {
+      this.prettyPrintVal = pretty
+      return this
+    }
+
+    /**
+     * Get if outline mode is enabled. Default is false. If enabled, the HTML output methods will consider
+     * all tags as block.
+     * @return if outline mode is enabled.
+     */
+    def outline: Boolean = outlineVal
+
+
+    /**
+     * Enable or disable HTML outline mode.
+     * @param outlineMode new outline setting
+     * @return this, for chaining
+     */
+    def outline(outlineMode: Boolean): Document.OutputSettings = {
+      this.outlineVal = outlineMode
+      this
+    }
+
+    /**
+     * Get the current tag indent amount, used when pretty printing.
+     * @return the current indent amount
+     */
+    def indentAmount: Int = indentAmountVal
+
+
+    /**
+     * Set the indent amount for pretty printing
+     * @param indentAmountIn number of spaces to use for indenting each level. Must be >= 0.
+     * @return this, for chaining
+     */
+    def indentAmount(indentAmountIn: Int): Document.OutputSettings = {
+      Validator.isTrue(indentAmountIn >= 0)
+      this.indentAmountVal = indentAmountIn
+      this
+    }
+
+    override def clone: Document.OutputSettings = {
+      var clone: Document.OutputSettings = null
+      try {
+        clone = super.clone.asInstanceOf[Document.OutputSettings]
+      }
+      catch {
+        case e: CloneNotSupportedException => {
+          throw new RuntimeException(e)
+        }
+      }
+      clone.charset(charset.name)
+      clone.escapeModeVal = this.escapeModeVal
+      clone
     }
   }
 
---
+++ src/test/scala/com/github/ligangty/scala/jsoup/nodes/DocumentTest.scala
@@ -0,0 +1,76 @@
+package com.github.ligangty.scala.jsoup.nodes
+
+import java.nio.charset.Charset
+
+import com.github.ligangty.scala.jsoup.nodes.Document.OutputSettings
+import com.github.ligangty.scala.jsoup.nodes.Entities.{EXTENDED, BASE}
+import org.scalatest.FunSuite
+
+/**
+ * Created by gli on 15-3-17.
+ */
+class Documentignore extends FunSuite {
+  ignore("setTextPreservesDocumentStructure") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreTitles") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreOutputEncoding") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreXhtmlReferences") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreNormalisesStructure") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreClone") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreClonesDeclarations") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreLocation() throws IOException") {
+    fail("not implemented!")
+  }
+
+  ignore("ignoreHtmlAndXmlSyntax") {
+    fail("not implemented!")
+  }
+
+  ignore("htmlParseDefaultsToHtmlOutputSyntax") {
+    fail("not implemented!")
+  }
+
+  // Ignored since this ignore can take awhile to run.
+  ignore("ignoreOverflowClone") {
+    fail("not implemented!")
+  }
+
+  test("Document.OutputSettings") {
+    val docSetting = new OutputSettings
+    assert(Charset.forName("UTF-8") == docSetting.charset)
+    assert(BASE == docSetting.escapeMode)
+    assert(docSetting.prettyPrint)
+    assert(!docSetting.outline)
+    assert(docSetting.indentAmount == 1)
+
+    docSetting.charset(Charset.forName("ISO-8859-1")).outline(true).escapeMode(EXTENDED).indentAmount(4).prettyPrint(false)
+
+    assert(Charset.forName("ISO-8859-1") == docSetting.charset)
+    assert(EXTENDED == docSetting.escapeMode)
+    assert(!docSetting.prettyPrint)
+    assert(docSetting.outline)
+    assert(docSetting.indentAmount == 4)
+
+  }
+
+}
